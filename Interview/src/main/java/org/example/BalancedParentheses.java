c3660a085c83e41112fada13fecd079935ba0a9b
lokeshsolanki871 <lokeshsolanki871@gmail.com>
2025-12-15 15:16:23 +0530

    core java practice

--- Interview/src/main/java/org/example/BalancedParentheses.java
+++ Interview/src/main/java/org/example/BalancedParentheses.java
@@ -1,4 +1,53 @@
 package org.example;
 
+import java.util.Stack;
+
 public class BalancedParentheses {
+
+    public static void main(String[] args) {
+
+        String str = "{[])}";
+        Stack<Character> stack = new Stack<>();
+
+        boolean isBalanced = true;
+
+        for (char ch : str.toCharArray()) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            // Closing brackets
+            else if (ch == ')' || ch == '}' || ch == ']') {
+
+                // Stack empty → invalid
+                if (stack.isEmpty()) {
+                    isBalanced = false;
+                    break;
+                }
+
+                char top = stack.pop();
+
+                // Matching check
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+
+        // Final check
+        if (!stack.isEmpty()) {
+            isBalanced = false;
+        }
+
+        if (isBalanced) {
+            System.out.println("Balanced Parentheses");
+        } else {
+            System.out.println("Not Balanced");
+        }
+    }
 }
---
+++ Interview/src/main/java/org/example/StringMethodds.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class StringMethodds {
+    static void main() {
+
+        Integer a = null;
+        int b = a;
+        System.out.println(b);
+
+    }
+}
